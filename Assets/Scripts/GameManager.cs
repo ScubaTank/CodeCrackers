4737db202dba8563da189c854d51e593e48a5033
BowVallyCA <c.anderson965@mybvc.ca>
2025-04-02 15:05:28 -0600

    Finished Code Scrambler
    After all this time our game about cracking codes finally has a code you have to crack. I finished the code scrambler so that it encodes the keyword correctly using own custom made code, the code was the last big thing left for me  to do so now ill be spending my time doing some final touches and etc.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,43 +11,122 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_Text _text;
 
     public string trueWord = "Fork";
-    private string codedWord = "";
+    //private string codedWord = "";
 
     private void Start()
     {
         SetColors();
 
-        //Scrambler();
+        Scrambler();
 
-        _text.SetText("Code: " + trueWord);
+
     }
 
     public string Scrambler()
     {
-        string codedWord = "";
 
-        int inArray = 0;
+        bool isThereRed = false;
 
-        //foreach (char charCount in trueWord)
-        //{
-        //    codedWord += trueWord[2];
-        //    inArray++;
-        //}
+        StringBuilder codedWord = new StringBuilder(trueWord);
 
-        codedWord += trueWord[2];
-        codedWord += trueWord[1];
-        codedWord += trueWord[4];
-        codedWord += trueWord[3];
+        for(int i = 3; i >= 0; i--)
+        {
+            //RED
+            if (_codeLight[i].color == _colorArray[0])
+            {
+                Debug.Log("Did red!");
+                //swap first two letters, and two last letters.
+                char tempchar = codedWord[0];
+                codedWord[0] = codedWord[1];
+                codedWord[1] = tempchar;
 
+                tempchar = codedWord[2];
+                codedWord[2] = codedWord[3];
+                codedWord[3] = tempchar;
 
-        return codedWord;
+                isThereRed = true;
+            }
+
+            //BLUE
+            if (_codeLight[i].color == _colorArray[1])
+            {
+                Debug.Log("Did blue!");
+                //first, check for a red
+                if (!isThereRed)
+                {
+                    foreach (Image c in _codeLight)
+                    {
+                        if (c.color == _colorArray[0])
+                        {
+                            isThereRed = true;
+                        }
+                    }
+                }
+
+                //there IS red
+                if (isThereRed)
+                {
+                    char tempchar = codedWord[i];
+                    codedWord[i] = codedWord[3];
+                    codedWord[3] = tempchar;
+                }
+            }
+
+            //GREEN
+            if (_codeLight[i].color == _colorArray[2])
+            {
+                Debug.Log("Did green!");
+                //if at the ends, swap them.
+                if (i == 0 || i == 3)
+                {
+                    char tempchar = codedWord[0];
+                    codedWord[0] = codedWord[3];
+                    codedWord[3] = tempchar;
+                }
+                else //otherwise, swap the middle.
+                {
+                    char tempchar = codedWord[1];
+                    codedWord[1] = codedWord[2];
+                    codedWord[2] = tempchar;
+                }
+            }
+
+            //YELLOW
+            if (_codeLight[i].color == _colorArray[3])
+            {
+
+                Debug.Log("Did yellow!");
+                if (i == 3)
+                {
+                    char tempchar = codedWord[0];
+                    codedWord[0] = codedWord[3];
+                    codedWord[3] = tempchar;
+                } 
+                else
+                {
+                    char tempchar = codedWord[i+1];
+                    codedWord[i+1] = codedWord[i];
+                    codedWord[i] = tempchar;
+                }
+
+                
+            }
+
+            Debug.Log("Current Word: " + codedWord.ToString());
+        }
+
+        Debug.Log("Is there a red? : " + isThereRed);
+
+        _text.SetText("Code: " + codedWord.ToString());
+
+        return codedWord.ToString();
     }
 
     public void SetColors()
     {
         foreach (Image count in _codeLight)
         {
-            int index = Random.Range(0, 3);
+            int index = Random.Range(0, 4);
 
             count.color = _colorArray[index];
         }
